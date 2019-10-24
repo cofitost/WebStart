c6139efd6346edce7fbeaa1a8f8b98507a27fc0f
cofitost <zxcv091922@gmail.com>
2019-10-25 03:19:58 +0800

    test webhook

--- src/test/test.js
+++ src/test/test.js
@@ -6,8 +6,6 @@ const testFile = 'index.html';
 const data = JSON.parse(fs.readFileSync(testCase));
 const mypath = "file:///var/lib/workspace/" + path.basename(path.resolve('../../')) + '/src/web/html/' + testFile;
 console.log(mypath);
-// const nums = data.id.split('.');
-// ++nums[2];
 data.suites[0].cases[0].records[0].target.options[0].value = mypath;
 
 fs.writeFileSync(testCase, JSON.stringify(data, null, 4));

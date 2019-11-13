fe2a1e2e893649e34ab1e6f4bc58da46559eae0a
cofitost <zxcv091922@gmail.com>
2019-11-13 23:35:08 +0800

    update

--- src/test/indexTest.js
+++ src/test/indexTest.js
@@ -11,12 +11,12 @@ const testFile = 'index.html'; //輸入受測作業檔名 ex:index.html
 describe('測試index.html', () => { //測試區塊，可輸入這個測試檔的目的 ex:測試index.html
 
   let capabilities = Capabilities.chrome();
-  capabilities.set("chrome.resolution","-screenwidth 1024 -screenheight 768")
-  const driver = new Builder().forBrowser('chrome').usingServer(seleniumServer).withCapabilities(capabilities).build(); //使用chrome browser來測試Web作業
+  const driver = new Builder().forBrowser('chrome').usingServer(seleniumServer).build(); //使用chrome browser來測試Web作業
   const dirPathNum = path.resolve(__dirname).split('/').indexOf('src') - 1; //ProgEdu系統判定作業，勿操作
 
   it('開啟作業網頁',async()=> {//此為Selenium server獲取local HTML file，勿操作
     await driver.get('file:///' + '/var/lib/workspace/' + path.resolve(__dirname).split('/')[dirPathNum] + '/src/web/html/' + testFile);
+    await driver.execute_script("document.body.style.zoom='2.0'")
     // await driver.get('http://140.134.26.76:28888');
   });
   

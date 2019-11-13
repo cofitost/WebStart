503692657b1e359c8e2b18e36f30b81c28757e74
cofitost <zxcv091922@gmail.com>
2019-11-13 17:10:36 +0800

    update

--- src/test/indexTest.js
+++ src/test/indexTest.js
@@ -11,13 +11,13 @@ const testFile = 'index.html'; //輸入受測作業檔名 ex:index.html
 describe('測試index.html', () => { //測試區塊，可輸入這個測試檔的目的 ex:測試index.html
 
   let capabilities = Capabilities.chrome();
-  capabilities.set("resolution",'1024x768')
+  capabilities.set("chrome.arguments","-screenwidth 1024 -screenheight 768")
   const driver = new Builder().forBrowser('chrome').usingServer(seleniumServer).withCapabilities(capabilities).build(); //使用chrome browser來測試Web作業
   const dirPathNum = path.resolve(__dirname).split('/').indexOf('src') - 1; //ProgEdu系統判定作業，勿操作
 
   it('開啟作業網頁',async()=> {//此為Selenium server獲取local HTML file，勿操作
-    await driver.get('file:///' + '/var/lib/workspace/' + path.resolve(__dirname).split('/')[dirPathNum] + '/src/web/html/' + testFile);
-    // await driver.get('http://140.134.26.76:28888');
+    // await driver.get('file:///' + '/var/lib/workspace/' + path.resolve(__dirname).split('/')[dirPathNum] + '/src/web/html/' + testFile);
+    await driver.get('http://140.134.26.76:28888');
   });
   
   it('測試input功能', async () => { //測試案例test case，可撰寫多個it並說明測試細節，有利於學生讀懂測試錯誤
